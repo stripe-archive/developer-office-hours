5d9525ad45bb9700127423af0aac08c46b3b36d9
Ali Riaz <aliriaz@stripe.com>
2019-12-03 14:27:29 -0600

    iOS one time payments example

---
+++ 2019-12-04-ios/ios/Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'one-time-payment' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Stripe', '18.2.0'
+  pod 'Alamofire', '4.9.1'
+
+  # Pods for one-time-payment
+
+end
---
+++ 2019-12-04-ios/ios/one-time-payment/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  one-time-payment
+//
+//  Created by Ali Riaz on 12/2/19.
+//  Copyright © 2019 Ali Riaz. All rights reserved.
+//
+
+import UIKit
+import Stripe
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        Stripe.setDefaultPublishableKey("pk_test_XBeVOiJfdrsz56X2j9qhMP59")
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
