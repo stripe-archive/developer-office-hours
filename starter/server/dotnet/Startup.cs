17f240f569d2d30fce029552a08e259a2d33f8de
CJ Avilla <cjavilla@stripe.com>
2020-11-16 17:42:31 -0800

    Update dotnet starter to remove https requirement

--- starter/server/dotnet/Startup.cs
+++ starter/server/dotnet/Startup.cs
@@ -53,7 +53,7 @@ namespace server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();\
             app.UseFileServer();
             app.UseRouting();
             app.UseAuthorization();
